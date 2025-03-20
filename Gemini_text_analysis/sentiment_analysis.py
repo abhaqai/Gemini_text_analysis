88ee0cab689cdbfe9bd2f7b60f864620b8779d35
Abdul Haque <abdul010haq@gmail.com>
2025-03-21 00:36:58 +0500

    Add files via upload

---
+++ Gemini_text_analysis/sentiment_analysis.py
@@ -0,0 +1,20 @@
+import google.generativeai as genai
+
+# Configure API Key
+genai.configure(api_key='AIzaSyCPCw8-RxzyEsN1QbFhS03w7pC8a2dgcnM')
+
+def sentiment_analysis(text):
+    model = genai.GenerativeModel("gemini-1.5-pro-latest")
+
+    response = model.generate_content(f"""You are trained to analyze and detect the sentiment of given text.
+                If you're unsure of an answer, you can say "not sure" and recommend users to review manually.
+                Analyze the following text and determine if the sentiment is: positive or negative.
+                Return answer in a single word as either positive or negative: {text}""")
+
+    response_text = response.text.strip().lower()
+    return response_text
+
+# Calling the function
+input_text = 'I love eating ice cream'
+response = sentiment_analysis(input_text)
+print(input_text, ': The Sentiment is', response)
